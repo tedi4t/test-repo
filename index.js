d5edfabb54bddef986602a357aa055232438ae6a
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2025-02-13 21:07:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+
+
+
+
+
+
+
+
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, "uploads");
